71e154e797c337f1a11d2c6d4596291c58d28df2
Shelby Diamond <Shelbydiamond518@gmail.com>
2019-08-26 16:14:17 -0700

    styled Login page,
    added global styles reset

--- better-professor/src/App.js
+++ better-professor/src/App.js
@@ -2,13 +2,16 @@ import React from "react"
 import "./App.css"
 import Login from "./components/Login"
 import PrivateRoute from "./utilities/PrivateRoute"
+import { Route, BrowserRouter as Router } from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <Route exact path="/" render={props => <Login {...props} />} />
-      <PrivateRoute path="/protected" component={SOMETHING} />
-    </div>
+    <Router>
+      <div className="App">
+        <Route exact path="/" render={props => <Login {...props} />} />
+        <PrivateRoute path="/protected" />
+      </div>
+    </Router>
   )
 }
 
