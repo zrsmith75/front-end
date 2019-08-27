75e7f3082e0b0ba45769a11b21570f8fdc408ade
Shelby Diamond <Shelbydiamond518@gmail.com>
2019-08-26 21:09:50 -0700

    updated a typo on the private route import

--- better-professor/src/App.js
+++ better-professor/src/App.js
@@ -1,14 +1,10 @@
 import React from "react"
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom"
 import "./App.css"
 import Login from "./components/Login"
 
-import PrivateRoute from "./utilities/privateRoute"
-import Registration from './components/Registration'
-
-
-
-
+import PrivateRoute from "./utilities/PrivateRoute"
+import Registration from "./components/Registration"
 
 function App() {
   return (
@@ -17,7 +13,7 @@ function App() {
         <header>
           <Link to="/registration">Sign up</Link>
         </header>
-        
+
         <Route exact path="/" render={props => <Login {...props} />} />
         <Route path="/registration" component={Registration} />
         <PrivateRoute path="/protected" />
